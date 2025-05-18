95991805386e6f53eb7eb7aa6a16b2eff9031e1f
Sidhanta Sharma <sidhantasharma41@gmail.com>
2025-05-18 00:55:12 -0700

    forgot some

---
+++ src/memory/memory_block.rs
@@ -0,0 +1,119 @@
+use std::{mem::size_of, usize};
+
+
+pub trait MemoryBlock {
+    fn writeu8(&mut self, address: usize, value: u8);
+    fn writeu16(&mut self, address: usize, value: u16);
+    fn writeu32(&mut self, address: usize, value: u32);
+    fn readu8(&self, address: usize) -> u8;
+    fn readu16(&self, address: usize) -> u16;
+    fn readu32(&self, address: usize) -> u32;
+}
+
+pub struct SimpleMemoryBlock {
+    pub memory: Vec<u8>,
+    memory_mask: usize,
+}
+
+impl SimpleMemoryBlock {
+    pub fn new(size: usize, mask: usize) -> Self {
+        SimpleMemoryBlock {
+            memory: vec![0; size],
+            memory_mask: mask,
+        }
+    }
+    fn get_memory_slice_mut<const SIZE: usize>(
+        &mut self,
+        address: usize,
+    ) -> Option<&mut [u8; SIZE]> {
+        let address = address & Self::get_slice_alignment(SIZE);
+        let mirror_masked_address = address & self.memory_mask;
+
+        let slice: &mut Vec<u8> = self.memory.as_mut();
+
+        if mirror_masked_address + SIZE > slice.len() {
+            return None;
+        };
+
+        Some(
+            slice[mirror_masked_address..][..SIZE]
+                .as_mut()
+                .try_into()
+                .unwrap(),
+        )
+    }
+
+    fn get_memory_slice<const SIZE: usize>(&self, address: usize) -> Option<&[u8; SIZE]> {
+        let address = address & Self::get_slice_alignment(SIZE);
+        let mirror_masked_address = address & self.memory_mask;
+
+        let slice: &Vec<u8> = self.memory.as_ref();
+
+        if mirror_masked_address + SIZE > slice.len() {
+            return None;
+        };
+
+        Some(
+            slice[mirror_masked_address..][..SIZE]
+                .as_ref()
+                .try_into()
+                .unwrap(),
+        )
+    }
+
+    const fn get_slice_alignment(size: usize) -> usize {
+        match size {
+            1 => !0x0,
+            2 => !0x1,
+            4 => !0x3,
+            _ => unreachable!(),
+        }
+    }
+}
+
+impl MemoryBlock for SimpleMemoryBlock {
+    fn writeu8(&mut self, address: usize, value: u8) {
+        let Some(slice) = self.get_memory_slice_mut::<{ size_of::<u8>() }>(address) else {
+            return;
+        };
+        slice.copy_from_slice(&value.to_le_bytes())
+    }
+
+    fn writeu16(&mut self, address: usize, value: u16) {
+        let Some(slice) = self.get_memory_slice_mut::<{ size_of::<u16>() }>(address) else {
+            return;
+        };
+        slice.copy_from_slice(&value.to_le_bytes())
+    }
+
+    fn writeu32(&mut self, address: usize, value: u32) {
+        let Some(slice) = self.get_memory_slice_mut::<{ size_of::<u32>() }>(address) else {
+            return;
+        };
+        slice.copy_from_slice(&value.to_le_bytes())
+    }
+
+    fn readu8(&self, address: usize) -> u8 {
+        let Some(slice) = self.get_memory_slice::<{ size_of::<u8>() }>(address) else {
+            return 0;
+        };
+
+        slice[0]
+    }
+
+    fn readu16(&self, address: usize) -> u16 {
+        let Some(slice) = self.get_memory_slice::<{ size_of::<u16>() }>(address) else {
+            return 0;
+        };
+
+        u16::from_le_bytes(*slice)
+    }
+
+    fn readu32(&self, address: usize) -> u32 {
+        let Some(slice) = self.get_memory_slice::<{ size_of::<u32>() }>(address) else {
+            return 0;
+        };
+
+        u32::from_le_bytes(*slice)
+    }
+}
---
+++ src/memory/oam_memory.rs
@@ -0,0 +1,84 @@
+use std::mem::size_of;
+
+use crate::graphics::wrappers::oam::Oam;
+
+use super::{
+    affine_parameters::{AffineParameters, NUM_OAM_AFFINE_PARAMETERS},
+    memory_block::{MemoryBlock, SimpleMemoryBlock},
+};
+pub struct OAMBlock {
+    memory: SimpleMemoryBlock,
+    affine_parameters: Vec<AffineParameters>,
+}
+const OAM_SIZE: usize = 0x400;
+const OAM_MIRROR_MASK: usize = 0x3FF;
+
+impl OAMBlock {
+    pub fn new() -> Self {
+        let oam = SimpleMemoryBlock::new(OAM_SIZE, OAM_MIRROR_MASK);
+        let mut affine_parameters = Vec::with_capacity(NUM_OAM_AFFINE_PARAMETERS);
+
+        for i in 0..NUM_OAM_AFFINE_PARAMETERS {
+            affine_parameters.push(AffineParameters::create_parameters(&oam, i))
+        }
+
+        Self {
+            memory: oam,
+            affine_parameters,
+        }
+    }
+
+    fn update_affine_paramters(&mut self, group: usize) {
+        self.affine_parameters[group] = AffineParameters::create_parameters(&self.memory, group);
+    }
+
+    pub fn get_affine_paramters(&self, group: usize) -> &AffineParameters {
+        &self.affine_parameters[group]
+    }
+
+    pub fn oam_read(&self, oam_num: usize) -> Oam<'_> {
+        let oam_slice: &[u8; 6] = self.memory.memory[oam_num * 0x08..][..6].try_into().unwrap();
+        let oam_slice: &[u16; 3] = unsafe { oam_slice.align_to::<u16>().1.try_into().unwrap() };
+
+        return Oam(oam_slice);
+    }
+
+    fn on_write_updates<const SIZE: usize>(&mut self, address: usize) {
+        for i in 0..SIZE {
+            // Check if affine parameters changed
+            if (address + i) & 0x6 == 0x6  {
+                let group = (address >> 5) & 0x1F;
+                self.update_affine_paramters(group);
+            }
+        }
+    }
+}
+
+impl MemoryBlock for OAMBlock {
+    fn writeu8(&mut self, address: usize, value: u8) {
+        self.memory.writeu8(address, value);
+        self.on_write_updates::<{size_of::<u8>()}>(address);
+    }
+
+    fn writeu16(&mut self, address: usize, value: u16) {
+        self.memory.writeu16(address, value);
+        self.on_write_updates::<{size_of::<u16>()}>(address);
+    }
+
+    fn writeu32(&mut self, address: usize, value: u32) {
+        self.memory.writeu32(address, value);
+        self.on_write_updates::<{size_of::<u32>()}>(address);
+    }
+
+    fn readu8(&self, address: usize) -> u8 {
+        self.memory.readu8(address)
+    }
+
+    fn readu16(&self, address: usize) -> u16 {
+        self.memory.readu16(address)
+    }
+
+    fn readu32(&self, address: usize) -> u32 {
+        self.memory.readu32(address)
+    }
+}
